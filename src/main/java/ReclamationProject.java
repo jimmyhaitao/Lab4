27bd641ddd5b6cf0afb15975a44b57d337593291
jimmyhaitao <jimmy.liao@ancss.org>
2017-10-04 00:55:44 -0500

    fixed checkstyle

--- src/main/java/ReclamationProject.java
+++ src/main/java/ReclamationProject.java
@@ -1,4 +1,4 @@
-/*
+/**
  * The following code needs a lot of TLC. So give it some!
  *
  * 1. Fix all checkstyle errors
@@ -8,19 +8,42 @@
  * 5. Remove unnecessary comments as appropriate
  */
 
-public class ReclamationProject
-{
-    static String doit(String a,String b){
-        if (a.length() > b.length()){
-            String c = a; // TODO: set c to a
-            a=b; b=c;}
-        String r = (a.equals(b)) ? "" : ""; // I love the ternary operator!
+
+public class ReclamationProject {
+    /**
+     *reclamation of a string a to b.
+     *@param a is a string
+     *@param b is a string
+     *@param c is a string
+     *@return return r
+     */
+    public static String reclamation(final String[] a, final String[] b, final String[] c) {
+        if (a.length > b.length) {
+            for (int l = 0; l < b.length; l++) {
+                b[l] = a[l];
+
+            }
+        }
+             String r = null;
+            if (a.equals(b)) {
+                r = " ";
+            } else {
+                r = " ";
+            }
         /*
          * For loop with i
          */
-        for (int i = 0; i < a.length(); i++) { for (int j = a.length() - i; j > 0; j--) {
-                for (int k = 0; k < b.length()- j; k++) {
-                    r = (a.regionMatches(i, b, k, j) && j >r.length()) ? a.substring(i,i + j) : r; // Do it!
-                        }} // Ah yeah
-        } return r; }
+        for (int i = 0; i < a.length; i++) {
+            for (int j = a.length - i; j > 0; j--) {
+                for (int k = 0; k < b.length - j; k++) {
+                    if (a.length > b.length && j > r.length() == true) {
+                        r = a[j];
+                    } else {
+                        return r;
+                    }
+                        }
+            }
+        }
+     return r;
+     }
 }
